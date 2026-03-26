5f8f6800252567e1c0a1747bcfbbb60c4262393d
Niariasve <niarias@espol.edu.ec>
2026-03-25 23:04:09 -0500

    Scaffold

---
+++ resources/js/pages/DataTable.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <div class="grid place-items-center h-dvh">
+        <div class="max-w-7xl mx-auto">
+            Hello World        
+        </div>
+    </div>
+</template>
+
+<style></style>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::inertia('/', 'Welcome')->name('home');
+
+Route::get('/data-table', function () {
+
+    return Inertia::render('DataTable');
+})->name('data-table');
