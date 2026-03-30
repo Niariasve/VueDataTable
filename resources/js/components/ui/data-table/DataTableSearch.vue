892650efa81780e1e94dcac705ee918aaa8314fd
Niariasve <niarias@espol.edu.ec>
2026-03-29 20:39:15 -0500

    Remove search animation

--- resources/js/components/ui/data-table/DataTableSearch.vue
+++ resources/js/components/ui/data-table/DataTableSearch.vue
@@ -30,44 +30,10 @@
         <Button @click="handleShow" variant="ghost" size="sm">
             <Search class="w-4 h-4" />
         </Button>
-        <Transition name="show">
-            <div v-show="show" class="search-shell">
-                <!-- <Input class="h-8 w-56" placeholder="Type to search"
-                    @update:model-value="table.setGlobalFilter($event.toString())" /> -->
-                <InputGroup class="h-8 w-56">
-                    <InputGroupInput placeholder="Type to search" />
-                </InputGroup>
-            </div>
-        </Transition>
+        <template v-show="show">
+            <InputGroup class="h-8 w-56">
+                <InputGroupInput placeholder="Type to search" @update:model-value="table.setGlobalFilter($event.toString())" />
+            </InputGroup>
+        </template>
     </div>
 </template>
-
-<style scoped>
-
-    .show-enter-active,
-    .show-leave-active {
-        transition:
-            width 300ms cubic-bezier(0.22, 1, 0.36, 1),
-            opacity 100ms ease,
-            transform 300ms cubic-bezier(0.22, 1, 0.36, 1);
-        transform-origin: left center;
-    }
-
-    .show-enter-from,
-    .show-leave-to {
-        width: 0;
-        opacity: 0;
-        transform: translateX(-6px);
-    }
-
-    .show-enter-to,
-    .show-leave-from {
-        width: 14rem;
-        opacity: 1;
-        transform: translateX(0);
-    }
-
-    .search-shell {
-        /* overflow: hidden; */
-    }
-</style>
